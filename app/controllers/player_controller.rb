c3b14e90874e0f8ad5fd90acc3f9bdf21d1c7389
Ellen Galperin <egalperi@EGALPERIN.local>
2016-09-15 15:50:54 -0400

    Ellen - refactor formatting

--- app/controllers/player_controller.rb
+++ app/controllers/player_controller.rb
@@ -7,31 +7,43 @@ class PlayerController < ApplicationController
   end
 
   def search
-    results
+    get_results
     render 'player/results'
   end
 
-  def results
+  def get_results
     RSpotify.authenticate('f6acf8947aa84d2b8266e571f344333d', '0a3da0d97956457cbc1e522667d89c14')
+
     playlists = RSpotify::Playlist.search('Top Hits')
 
-    shuffle(playlists)
+    playlists.each do | playlist |
+      matches_search = playlist.name.include? 'Todays Top Hits'
+      if matches_search
+        tracks = playlist.tracks
+        list_of_track_uris = shuffle(get_uris(tracks))
 
-    playlists.each do |playlist|
-      include=playlist.name.include? 'Todays Top Hits'
-      if (include)
-        @url = 'https://embed.spotify.com/?uri=' + playlist.uri
+        playlist_url = 'https://embed.spotify.com/?uri=spotify:trackset:PREFEREDTITLE:'
+
+        list_of_track_uris.each do |trackUri|
+          playlist_url += trackUri + ','
+        end
+
+        @url = playlist_url
       end
     end
   end
 
+  def get_uris(list_of_tracks)
+    list_of_uris = []
+    list_of_tracks.each do |track|
+      track_uri = track.uri[/((?<=track:)([^\n\r]*))/]
+      list_of_uris.push(track_uri)
+    end
+    list_of_uris
+  end
+
   def shuffle(list)
-    puts 'BEFORE ******************************'
-    puts list
-    list = list.shuffle
-    puts 'AFTER ******************************'
-    puts list
-    list
+    list.shuffle
   end
 
 end
